e676d1f256b41f4b425084723e90b0708f3ce1f8
oesteco <vilarinho46@gmail.com>
2025-05-10 09:26:45 -0400

    equipe

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -39,7 +39,7 @@ const App = () => {
     {
       nome: 'Ana Luiza',
       local: 'Ribeirão Preto (SP)',
-      texto: 'Fiz 3 anos de cursinho no Brasil. Aqui entrei direto, já estou no 4º semestre com estágio e prática.',
+      texto: 'Tentei vestibular no Brasil por 3 anos sem sucesso. Com a equipe do 'Quero Ser Médico', em apenas 3 meses já estava matriculada e assistindo aulas. O suporte para documentação e moradia foi essencial.',
       foto: 'https://i.pravatar.cc/150?img=3'
     },
     {
